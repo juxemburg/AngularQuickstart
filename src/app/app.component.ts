4e5a46dda099a9b16ce97fd6b62832f1518f6f36
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2017-08-24 15:38:04 -0500

    Chapter 3 and 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,25 @@
 import { Component } from '@angular/core';
+import { Customer, Address } from './model';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-  <h1>Hello {{name}}</h1>
-  <p><i>{{name}} is from {{region}}</i></p><br>
-  <input [value]="name"><br>
-  <input [(ngModel)]="name">
-
-  <label><input type="checkbox" [(ngModel)]="hideAddress" >Hide Address</label>
-  <div [hidden]="hideAddress">
-    <fieldset>
-      <label>street:</label>
-      <input [(ngModel)]="street">
-    </fieldset>
-    <fieldset>
-      <label>city:</label>
-      <input [(ngModel)]="city">
-    </fieldset>
-  </div>
-  <select [(ngModel)]="region">
-    <option>North</option>
-    <option>South</option>
-    <option>West</option>
-    <option>East</option>
-  </select>
-  `,
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent  { 
-  name:string = 'Alex Smith'; 
-  street:string = '123 main street';
-  city:string ='Anytown';
-  region:string;
-  hideAddress:boolean = false;
+export class AppComponent {
+  
+
+  customer: Customer = { 
+    id: 1,
+    name: '',
+    address: {
+      city: 'Anytown',
+      region: 'east',
+      street:'123 main street',
+      state: 'California'
+    }
+  };
+  hideAddress: boolean = false;
 
 }
